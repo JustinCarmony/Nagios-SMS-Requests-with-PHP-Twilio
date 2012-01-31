923b1d2a06805cba5f3e2d910e161673c513dde9
Justin Carmony <justin@justincarmony.com>
2012-01-30 22:17:42 -0700

    Tweak

--- sendTxtMsg.php
+++ sendTxtMsg.php
@@ -2,19 +2,25 @@
 
 require('twilio-php/Services/Twilio.php');
 
+/* Start Configs */
+
+$sid = "A123.....";
+$token = "29d6b9f.......";
+$twilio_number = '4045550101';
+
+/* End Configs */
+
 # Get the Argvs
 $phone  = $argv[1];
 $msg    = $argv[2];
 $msg    = str_replace('\n', "\n", $msg);
 
-$sid = "A123.....";
-$token = "29d6b9f.......";
 
 $client = new Services_Twilio($sid, $token);
 try 
 {
     $message = $client->account->sms_messages->create(
-        '8015553411', // From a valid Twilio number
+        $twilio_number, // From a valid Twilio number
         $phone, // Text this number
         $msg
     );    
